53a19d9d11f51fc4d4293f4aa657f076ceadb1f8
Rod B <Rodbee_2015@msn.com>
2018-02-24 14:34:06 -0500

    {my 1st project)

--- features/scenario.feature
+++ features/scenario.feature
@@ -3,7 +3,7 @@ Feature: Simple maths
   As a developer
   I want to increment variables
 
-  Scenario: easy maths
-    Given I go to duck duck go
-    When  I search for WebdriverIO
-    Then  I should see the search results
+Scenario: easy maths
+    Given I go to google
+    When  I search for WebdriverIO google
+    Then  I should see the search results google
--- features/support/steps.js
+++ features/support/steps.js
@@ -30,3 +30,31 @@ Then('I should see the search results', function(callback) {
     callback(error);
   })
 })
+
+Given('I go to google', function(callback) {
+  this.browser
+    .init()
+    .url('https://google.com/').then(function() {
+      callback();
+    })
+})
+
+When('I search for WebdriverIO google' , function(callback) {
+  this.browser
+    .setValue('#lst-ib', 'WebdriverIO')
+    .keys('Enter').then(function(){
+      callback();
+    }).catch(function(error){
+    callback(error);
+  })
+})
+
+Then('I should see the search results google', function(callback) {
+  this.browser
+    .getTitle().then(function(result){
+        result.should.equal("WebdriverIO - Google Search");
+        callback();
+  }).catch(function(error){
+    callback(error);
+  })
+})
